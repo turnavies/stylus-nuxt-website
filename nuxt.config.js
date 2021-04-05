8e7417c1be6e4d0873feab87e2b68a32ab702f35
Thomas Van Oekelen <thomas.vanoekelen@gmail.com>
2021-04-05 15:59:58 +0200

    fix site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: config.title || '',
+    title: config.title.map(i => {return i}).join(" ") || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
