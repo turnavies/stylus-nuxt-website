96cc3ad725bf5f435e74811c29f41c882c962737
Thomas Van Oekelen <thomas.vanoekelen@gmail.com>
2021-04-07 23:53:32 +0200

    indent index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,23 +7,23 @@
 </template>
 
 <script>
-import ProjectList from '~/components/ProjectList.vue'
+    import ProjectList from '~/components/ProjectList.vue'
 
-export default {
-  components: {
-    ProjectList
-  },
-  layout: 'layout',
-  async asyncData() {
-      // create context via webpack to map over all project posts
-      const allPosts = await require.context("~/content/projects/", true, /\.md$/)
-      const posts =  allPosts.keys().map((key) => {
-        // give back the value of each post context
-        return allPosts(key)
-      });
-      return {
-        posts
+    export default {
+      components: {
+        ProjectList
+      },
+      layout: 'layout',
+      async asyncData() {
+          // create context via webpack to map over all project posts
+          const allPosts = await require.context("~/content/projects/", true, /\.md$/)
+          const posts =  allPosts.keys().map((key) => {
+            // give back the value of each post context
+            return allPosts(key)
+          });
+          return {
+            posts
+          }
       }
-  }
-}
+    }
 </script>
